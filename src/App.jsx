d0982f0a1235f30190e1397754b59e54f3e70a6f
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-10 16:50:26 +0200

    feat: intestazione fatta

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 // importazione file
 import { useState } from 'react'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 import films from './data/film'
 
 function App() {
 
   return (
     <>
-      
+      <Header/>
+      <Main />
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="container">
+                <h1 className="title-header">Lista film da vedere</h1>
+            </div>
+        </header>
+    )
+}
